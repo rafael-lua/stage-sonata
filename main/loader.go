a4dc8ee5c66bd62ea3654e0ac3a0e4d5d83781e0
rafael-lua <rafael.tragueta79@gmail.com>
2022-09-24 18:18:19 -0300

    feat: add basic stage select

---
+++ main/loader.go
@@ -0,0 +1,51 @@
+components {
+  id: "loader"
+  component: "/main/loader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_main_menu"
+  type: "collectionproxy"
+  data: "collection: \"/main/scenes/main_menu/main_menu.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_game_stage"
+  type: "collectionproxy"
+  data: "collection: \"/main/scenes/game_stage/game_stage.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
